1e509772dae38f6bac48e8754ffe6c1acfc4512f
timhunuig <Timothy.Hulshof@nuigalway.ie>
2021-12-02 15:13:00 +0000

    plotly and explanations added

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime
 import pandas as pd
 from fastai.tabular.all import *
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.metrics import precision_score, recall_score, accuracy_score
 import matplotlib.pyplot as plt
 from scipy.cluster import hierarchy as hc
@@ -17,6 +17,8 @@ import seaborn as sns
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
 import umap.umap_ as umap
+
+import plotly.express as px
 
 #import os
 
@@ -123,10 +125,10 @@ def random_forest_analysis(file, dep_var, reduction_method="accuracy", bound=0.0
                     to_drop.append(lst[i])
                 else:
                     pass
-            
-        #plt.savefig(nextnonexistent(path_to_folder_other + "/top_features_dendrogram.png"))
+
+        plt.title("Similarity Dendrogram", fontsize=font_size_title)    
         st.pyplot(fig)
-        
+
         return to_drop
     
     #UMAP Fitting Function
@@ -154,7 +156,37 @@ def random_forest_analysis(file, dep_var, reduction_method="accuracy", bound=0.0
             hours = int(minutes // 60)
             return str(hours) + " hours " + str(minutes - hours * 60) + " minutes " + str(round(time_in_seconds - minutes * 60)) + " seconds"
     
+    def confusion_matrices_generation():
+        with st.spinner("Generating confusion matrices..."):   
+
+            #confusion matrices 
+            col1, col2, = st.columns(2)
+            with col1:
+                #st.header("Confusion matrix, without normalization")
+                ConfusionMatrixDisplay.from_predictions(valid_y, preds, 
+                                                        display_labels=classnames, 
+                                                        normalize=None, 
+                                                        xticks_rotation=67.5, 
+                                                        colorbar=False, 
+                                                        cmap=plt.cm.Blues)
+                plt.title("Confusion matrix, without normalization")
+                st.pyplot(fig=plt)
+            with col2:
+                #st.header("Normalized confusion matrix")
+                ConfusionMatrixDisplay.from_predictions(valid_y, preds, 
+                                                        display_labels=classnames, 
+                                                        normalize='true', 
+                                                        xticks_rotation=67.5, 
+                                                        colorbar=False, 
+                                                        cmap=plt.cm.Blues)
+                plt.title("Normalized confusion matrix")
+                st.pyplot(fig=plt)
+            
+            elapsed_time = time.perf_counter() - t
+        st.success('Confusion matrices generated! Time elapsed: {}'.format(output_time(elapsed_time)))
     ##### Script Start #####
+
+    font_size_title=16
 
     with st.spinner("Reading uploaded file..."):
         df = pd.read_csv(file)
@@ -212,29 +244,7 @@ def random_forest_analysis(file, dep_var, reduction_method="accuracy", bound=0.0
     accuracy = round(accuracy_score(valid_y, preds)*100, 2)
     st.write(pd.DataFrame({'Score': [precision, recall, accuracy]}, index=["Precision", "Recall", "Accuracy"]))
 
-    with st.spinner("Generating confusion matrices..."):    
-        #confusion matrices 
-        try:
-            figwidth = figsizecalc(len(classnames))*4 + 8 #weight & bias needed as figures are grouped in one plot
-            fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(figwidth, figwidth))
-            for ms, ax in zip([["Confusion matrix, without normalization", None], ["Normalized confusion matrix", 'true']], axes.flatten()):
-                disp = plot_confusion_matrix(m, valid_xs, valid_y,
-                                            display_labels=classnames,
-                                            cmap=plt.cm.Blues,
-                                            ax=ax,
-                                            normalize=ms[1],
-                                            xticks_rotation=67.5,
-                                            colorbar=False) 
-
-                disp.ax_.set_title(ms[0])
-        
-            plt.tight_layout()
-            st.pyplot(fig)
-        except:
-            pass
-        
-        elapsed_time = time.perf_counter() - t
-    st.success('Confusion matrices generated! Time elapsed: {}'.format(output_time(elapsed_time)))
+    confusion_matrices_generation()
     with st.spinner("Generating model with important features..."):
         #feature importance all features
         fi = rf_feat_importance(m, xs)
@@ -244,13 +254,14 @@ def random_forest_analysis(file, dep_var, reduction_method="accuracy", bound=0.0
         else:
             top_feature_length = 30
         
-        #Plotting Function for Feature Importance
-        fi[:top_feature_length].plot('features', 'imp', 'barh', figsize=(12,7), legend=False)
-        
         title = "Top " + str(top_feature_length) + " Features"
-        
-        plt.title(title)
-        st.pyplot(fig=plt)
+        fig = px.histogram(fi[:top_feature_length], x="imp", y="features")
+        fig.update_layout(
+            title=title,
+            xaxis_title="Feature Importance",
+            yaxis_title="Features",
+        )
+        st.plotly_chart(fig)
         
         m_imp, xs_imp, valid_xs_imp, to_keep = data_reduction(m, xs, y, valid_xs, valid_y, fi, reduction_method, bound)
         
@@ -260,12 +271,13 @@ def random_forest_analysis(file, dep_var, reduction_method="accuracy", bound=0.0
 
     preds = m_imp.predict(valid_xs_imp)
     st.write(f"##### Reduction in features: {len(xs.columns)} " +  r"""$$\rightarrow$$""" + f" {len(xs_imp.columns)}")
-    st.write("###### Model Metrics with important reactions only")
+    st.write(f"##### Model Metrics with the most important {len(xs_imp.columns)} features:")
     precision = round(precision_score(valid_y, preds, average='macro')*100, 2)
     recall = round(recall_score(valid_y, preds, average='macro')*100, 2)
     accuracy = round(accuracy_score(valid_y, preds)*100, 2)
     st.write(pd.DataFrame({'Score': [precision, recall, accuracy]}, index=["Precision", "Recall", "Accuracy"]))
 
+    confusion_matrices_generation()
     with st.spinner("Checking for redundant features..."):
 
         #feature importance important features
@@ -277,7 +289,6 @@ def random_forest_analysis(file, dep_var, reduction_method="accuracy", bound=0.0
         
         elapsed_time = time.perf_counter() - t
     st.success('Redundancy check done! Time elapsed: {}'.format(output_time(elapsed_time)))
-
     with st.spinner("Training final model..."):
         
         #remove redundant reactions from to_keep
@@ -297,12 +308,13 @@ def random_forest_analysis(file, dep_var, reduction_method="accuracy", bound=0.0
     preds = m_final.predict(valid_xs_final)
     
     st.write(f"##### Reduction in features: {len(xs_imp.columns)} " +  r"""$$\rightarrow$$""" + f" {len(xs_final.columns)}")
-    st.write("###### Model Metrics with Final Features")
+    st.write(f"##### Model Metrics with the final {len(xs_final.columns)} features:")
     precision = round(precision_score(valid_y, preds, average='macro')*100, 2)
     recall = round(recall_score(valid_y, preds, average='macro')*100, 2)
     accuracy = round(accuracy_score(valid_y, preds)*100, 2)
     st.write(pd.DataFrame({'Score': [precision, recall, accuracy]}, index=["Precision", "Recall", "Accuracy"]))
 
+    confusion_matrices_generation()
     #feature importance final features
     fi = rf_feat_importance(m_final, xs_final)
     
@@ -310,19 +322,17 @@ def random_forest_analysis(file, dep_var, reduction_method="accuracy", bound=0.0
 
         #heatmap
         c_mat = xs_final.corr()
-        figwidth = figsizecalc(len(xs_final.columns))
-        fig = plt.figure(figsize=(figwidth, figwidth))
-        
-        title = "Final Features Heatmap"
-        plt.title(title)
-        sns.heatmap(c_mat, vmax = .8, square = True)
-        
-        plt.tight_layout()
-        st.pyplot(fig)
+        c_mat_df = pd.DataFrame(c_mat, index=xs_final.columns, columns=xs_final.columns)
+        fig = px.imshow(c_mat_df, title="Final Features Heatmap")
+        fig.update_layout(width=800, height=800)
+        fig.update_xaxes(
+        tickangle = 90)
+        st.plotly_chart(fig)
     
         #histogram  
-        xs_final.hist(figsize = (figwidth, figwidth))
+        xs_final.hist(figsize = (figwidth, figwidth), )
         
+        plt.suptitle("Histogram for each Final Feature", y=1.02, fontsize=font_size_title)
         plt.tight_layout()
         st.pyplot(fig=plt)
         
@@ -365,8 +375,14 @@ def random_forest_analysis(file, dep_var, reduction_method="accuracy", bound=0.0
 
         sc.legend_.set_title(None)
 
-        plt.title("LDA 2D")
+        plt.title("LDA 2D", fontsize=font_size_title)
         st.pyplot(fig=plt)
+
+        fig = px.scatter(LDA_df, x="l1", y="l2", color=dep_var+" label", title="Interactive LDA 2D")
+        fig.update_traces(marker=dict(line=dict(width=0.8,
+                                    color='White')),
+                selector=dict(mode='markers'))
+        st.plotly_chart(fig)
         
         #lda 3d
         cmap = ListedColormap(sns.color_palette("hls", len(classnames)).as_hex())
@@ -398,8 +414,14 @@ def random_forest_analysis(file, dep_var, reduction_method="accuracy", bound=0.0
         leg = ax.legend(handles=sc.legend_elements(alpha=1)[0], labels=true_class_names, loc='upper left', bbox_to_anchor=(1.05, 1)) #, bbox_to_anchor=(1.0125, 1), loc=2
         
         plt.tight_layout()
-        plt.title("LDA 3D")
+        plt.title("LDA 3D", fontsize=font_size_title)
         st.pyplot(fig=plt)
+
+        fig = px.scatter_3d(LDA_df_3d, x="l1", y="l2", z="l3", color=dep_var+" label", title="Interactive LDA 3D")
+        fig.update_traces(marker=dict(line=dict(width=0.8,
+                                    color='White')),
+                selector=dict(mode='markers'))
+        st.plotly_chart(fig)
         
         elapsed_time = time.perf_counter() - t
     st.success('LDA done! Time elapsed: {}'.format(output_time(elapsed_time)))
@@ -426,8 +448,14 @@ def random_forest_analysis(file, dep_var, reduction_method="accuracy", bound=0.0
 
             sc.legend_.set_title(None)
 
-            plt.title("UMAP 2D: n_neighbours={}".format(n))
+            plt.title("UMAP 2D: n_neighbours={}".format(n), fontsize=font_size_title)
             st.pyplot(fig=plt)
+
+            fig = px.scatter(umap_df_2d, x="u1", y="u2", color=dep_var+" label", title="Interactive UMAP 2D")
+            fig.update_traces(marker=dict(line=dict(width=0.8,
+                                        color='White')),
+                  selector=dict(mode='markers'))
+            st.plotly_chart(fig)
             
             #umap 3d
             umap_result = draw_umap(n, 0.1, 3, df_final)
@@ -460,8 +488,14 @@ def random_forest_analysis(file, dep_var, reduction_method="accuracy", bound=0.0
             ax.legend(handles=sc.legend_elements(alpha=1)[0], labels=true_class_names, loc='upper left', bbox_to_anchor=(1.05, 1)) #, bbox_to_anchor=(1.0125, 1), loc=2
             
             plt.tight_layout()
-            plt.title("UMAP 3D: n_neighbours={}".format(n))
+            plt.title("UMAP 3D: n_neighbours={}".format(n), fontsize=font_size_title)
             st.pyplot(fig=plt)
+
+            fig = px.scatter_3d(umap_df, x="u1", y="u2", z="u3", color=dep_var+" label", title="Interactive UMAP 3D")
+            fig.update_traces(marker=dict(line=dict(width=0.8,
+                                        color='White')),
+                  selector=dict(mode='markers'))
+            st.plotly_chart(fig)
 
             elapsed_time = time.perf_counter() - t
         st.success('UMAP done! Time elapsed:: {}'.format(output_time(elapsed_time)))
@@ -479,25 +513,75 @@ def random_forest_analysis(file, dep_var, reduction_method="accuracy", bound=0.0
 
 ##streamlit start##
 
-st.title("Random Forest Analysis")
-
 st.write("""
-## Data reduction through random forests
+# Random Forest Analysis
+## *Data analysis and reduction through random forests*
 """)
 
+with st.expander("See explanation"):
+    st.write("""
+    Machine learning is an effective tool to classify but also infer feature (an individual measurable property of the dataset) importance. 
+    Knowledge of feature importance leads to reduced data size and a clearer picture of what features are inherent to and defining of specific classifications.
+
+    Recent studies have shown that structured data can be best modeled by ensembles of decision trees such as random forest or gradient boosting machines. 
+    Random forests train faster than gradient boosting machines and requires significantly less hyperparameter tuning. 
+    Although gradient boosting machines yield slightly better results, they are better used as a tool for specific probing. 
+    Random forests provide a great “one size fits all” approach.
+
+    To discover the minimum required data to effectively classify data, feature importance needs to be gauged. 
+    In a random forest, feature importance is calculated by the decrease in node impurity which is a measure of how well the node splits the data, weighted by the probability of reaching that node. 
+    The probability of reaching a node is calculated by the number of data entries that reach that specific node, divided by the total number of data entries. 
+    The most important features have the highest values. If the most important features are known the dataset can be reduced to these features and maintain levels accuracy, recall and precision that remain close to the initial model’s levels which is trained on the entire dataset. 
+
+    To do this new models must be continuously trained on a growing number of important features until these metrics are within acceptable bounds of the metric standards of the initial model. 
+    This is where random forest’s fast training time is of particular use and other machine learning models become obsolete. 
+
+    Once an optimum amount of important features are established, any potential redundancy of these features can be found by determining similarity. 
+    This is done by calculating the Spearman’s Rank-Order Correlation for the features. All but one of the closely related features can be removed with minimal impact to accuracy, thus, reducing the dataset size furthermore.
+    The resulting reduction in the size of the dataset can be seen as reduction in the dimensionality of the dataset and now more easily lends itself to visualization techniques.
+    Linear Discriminant Analysis is used to find an optimum representation of the reduced dataset in three dimensional space. 
+    UMAP is used to visually show if the dataset has inherent divisibility.
+    """)
+
+with st.expander("Sidebar documentation"):
+    st.write("""
+    - *Reduction Method*: During the random forest continuous retraining phase, the stopping point comes when either the accuracy is within
+    bound of the initially trained model's accuracy, or if accuracy, recall and precision are all within this bound of their respective 
+    initially trained model's counterpart. 
+    What is recommended is that if your data contains low amount of training samples for a specific class, then have the *reduction method* set to just accuracy
+    is preferable, as recall and precision are most likely to be not high in this scenario. If this is not the case setting the *reduction method* to
+    accuracy, precision and recall is then preferable. 
+
+    - *Bound*: This is the *bound* described in *reduction method* above. 
+
+    - *UMAP*: "Uniform Manifold Approximation and Projection (UMAP) is a dimension reduction technique that can be used for visualisation similarly to t-SNE"
+    \- https://umap-learn.readthedocs.io/en/latest/.
+    It is optional as depending on the size of the data can take considerably long time to generate. 
+
+    - *n_neighbours (UMAP)*: This setting is for *UMAP* and it is described that the greater the *n_neighbours* value the more is revealed about the
+    global structure of the data, whereas, a small *n_neighbours* value reveals more the local structure.
+    Default is **250** which is high enough in most cases to be considered to reveal the global structure.  
+    """)
 reduction_method = st.sidebar.selectbox("Reduction Method", ("accuracy", "accuracy, precision and recall"))
-umap_op = st.sidebar.selectbox("UMAP", ("false", "true"))
-n = st.sidebar.number_input("n_neighbours (UMAP)", min_value=0, value=250)
+bound = st.sidebar.slider("Bound (10^-4)", min_value=1, max_value=10, value=5, step=1)
+umap_op = st.sidebar.selectbox("UMAP", ("true", "false"))
+n = st.sidebar.slider("n_neighbours (UMAP)", min_value=0, max_value=1000, value=250)
 
 file = st.file_uploader("Upload file", type=['csv'])
 
 if not file:
     st.write("**Upload file to get started**")
 else:
+    # if len(file) > 1:
+    #     st.warning(f"Maximum number of files reached. Only the first will be processed.")
+    #     file = file[0]
+
     dep_var = st.text_input("Input dependent variable")
+
     if not dep_var:
         st.write("**Enter dependent variable to move on**")
     else:
-        kept_features = random_forest_analysis(file, dep_var, reduction_method=reduction_method, umap_op=umap_op, n=n, idx="random")
+        kept_features = random_forest_analysis(file, dep_var, reduction_method=reduction_method, umap_op=umap_op, n=n, bound=bound*10**-4, idx="random")
+        st.write(f"Final features are: {kept_features}")
 
 
